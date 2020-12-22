2e512e418806a490005cb2f34aa155e2f2d2ecf8
Pierson Lee (PIE) <piel@microsoft.com>
2020-12-21 19:31:35 -0800

    Days 7/8

---
+++ 2020/7/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _7
+{
+    class Program
+    {
+        static Regex bagLineRegex = new Regex(@"^(?<bagColor>.*)\sbags contain(?<contents>.*)$");
+        static Regex contentsRegex = new Regex(@"(?<count>[0-9]+)\s(?<bagColor>[^,.]+)\sbag(s{0,1})");
+
+        static void Main(string[] args)
+        { 
+            var input = File.ReadAllText("input.txt").Trim(' ');
+            var bagLines = input.Split("\r\n");
+            var baglistA = GetContainingBagsA(bagLines);
+
+            PartA(baglistA);
+
+            var baglistB = GetBagsForPartB(bagLines);
+            PartB(baglistB);
+            Debugger.Break();
+        }
+
+
+        private static Dictionary<string, Dictionary<string, int>> GetContainingBagsA(string[] bags)
+        {
+            Dictionary<string, Dictionary<string, int>> bagTypes = new Dictionary<string, Dictionary<string, int>>();
+            foreach (var line in bags)
+            {
+                Match match = bagLineRegex.Match(line);
+                string bagColor = match.Groups["bagColor"].Value;
+
+                foreach (Match cMatch in contentsRegex.Matches(match.Groups["contents"].Value))
+                {
+                    string cColor = cMatch.Groups["bagColor"].Value;
+                    int cnt = Int32.Parse(cMatch.Groups["count"].Value);
+
+                    if (!bagTypes.TryGetValue(cColor, out Dictionary<string, int> cValues))
+                    {
+                        cValues = new Dictionary<string, int>();
+                        bagTypes.Add(cColor, cValues);
+                    }
+
+                    cValues.Add(bagColor, cnt);
+                }
+            }
+
+            return bagTypes;
+        }
+
+        private static void PartA(Dictionary<string, Dictionary<string, int>> bagList)
+        {
+            List<string> containingBag = new List<string>();
+            Stack<string> bagWalkList = new Stack<string>();
+
+            foreach (var color in bagList["shiny gold"])
+            {
+                containingBag.Add(color.Key);
+                bagWalkList.Push(color.Key);
+            }
+
+            while (bagWalkList.Count > 0)
+            {
+                var color = bagWalkList.Pop();
+                if (bagList.TryGetValue(color, out Dictionary<string, int> containingColors))
+                {
+                    foreach (var cColor in containingColors.Keys)
+                    {
+                        if (!containingBag.Contains(cColor))
+                        {
+                            containingBag.Add(cColor);
+                            bagWalkList.Push(cColor);
+                        }
+                    }
+                }
+            }
+
+            int counter = containingBag.Count;
+            Console.WriteLine($"A: Number of bags that can have a shiny gold is {counter}.");
+        }
+        
+        static Dictionary<string, Dictionary<string, int>> GetBagsForPartB(string[] baglines)
+        {
+            Dictionary<string, Dictionary<string, int>> baglist = new Dictionary<string, Dictionary<string, int>>();
+            foreach(var bag in baglines)
+            {
+                var match = bagLineRegex.Match(bag);
+                var key = match.Groups["bagColor"].Value;
+                var contents = new Dictionary<string, int>();
+                foreach (Match containingBag in contentsRegex.Matches(match.Groups["contents"].Value))
+                {
+                    contents.Add(containingBag.Groups["bagColor"].Value, Int32.Parse(containingBag.Groups["count"].Value));
+                }
+
+                baglist[key] = contents;
+            }
+
+            return baglist;
+        }
+
+        static void PartB(Dictionary<string, Dictionary<string, int>> bagList)
+        {
+            int counter = GetBagCount("shiny gold", bagList);
+            Console.WriteLine($"B: {counter} bags fit in the shiny gold bag.");
+        }
+
+        /// <summary>
+        /// Recursive function to add the number of bags itself contains. 
+        /// </summary>
+        /// <param name="bagColor"></param>
+        /// <param name="bagList"></param>
+        /// <returns></returns>
+        private static int GetBagCount(string bagColor, Dictionary<string, Dictionary<string, int>> bagList)
+        {
+            int counter = 0;
+            if (bagList.ContainsKey(bagColor))
+            {
+                foreach (var bag in bagList[bagColor])
+                {
+                    counter += bag.Value;
+                    counter += bag.Value * GetBagCount(bag.Key, bagList);
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ 2020/8/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt").Trim(' ').Split("\r\n");
+
+            PartA(input);
+            PartB(input);
+            Debugger.Break();
+        }
+
+        private static Regex lineRegex = new Regex(@"^(?<action>.*)\s(?<sign>[+-]{1})(?<count>[0-9]+)$");
+        private static void PartA(string[] input)
+        {
+            int value = 0;
+
+            bool[] traversal = new bool[input.Length];
+            int pos = 0;
+            while (!traversal[pos])
+            {
+                traversal[pos] = true;
+                Match match = lineRegex.Match(input[pos]);
+
+                switch (match.Groups["action"].Value)
+                {
+                    case "nop":
+                        pos++;
+                        break;
+                    case "jmp":
+                        pos += GetValue(match.Groups["sign"].Value, match.Groups["count"].Value);
+                        break;
+                    case "acc":
+                        value += GetValue(match.Groups["sign"].Value, match.Groups["count"].Value);
+                        pos++;
+                        break;
+                    default:
+                        Debugger.Break();
+                        break;
+                }
+            }
+
+            Console.WriteLine($"A: Value is {value}");
+        }
+
+        static void PartB(string[] input)
+        {
+            int pos = 0;
+            int value;
+            while (!Traverse(pos, input, out value))
+            {
+                pos++;
+            }
+
+            Console.WriteLine($"B: Value is {value}");
+        }
+
+        private static bool Traverse(int curPos, string[] input, out int val)
+        {
+            val = 0;
+            int value = 0;
+            int length = input.Length;
+            bool[] traversal = new bool[length];
+            int  pos = 0;
+            while (pos < length && pos >= 0 && !traversal[pos])
+            {
+                traversal[pos] = true;
+                Match match = lineRegex.Match(input[pos]);
+                switch (match.Groups["action"].Value)
+                {
+                    case "nop":
+                        if (curPos == pos)
+                        {
+                            // treat it as a jump
+                            pos += GetValue(match.Groups["sign"].Value, match.Groups["count"].Value);
+                            continue;
+                        }
+                        pos++;
+                        break;
+                    case "jmp":
+                        if (curPos == pos)
+                        {
+                            pos++;
+                            break;
+                        }
+                        pos += GetValue(match.Groups["sign"].Value, match.Groups["count"].Value);
+                        break;
+                    case "acc":
+                        value += GetValue(match.Groups["sign"].Value, match.Groups["count"].Value);
+                        pos++;
+                        break;
+                    default:
+                        Debugger.Break();
+                        break;
+                }
+            }
+
+            if (pos < length)
+                return false;
+
+            val = value;
+            return true;
+        }
+
+        private static int GetValue(string sign, string count)
+        {
+            int val = Int32.Parse(count);
+            switch (sign)
+            {
+                case "+":
+                    return val;
+                case "-":
+                    return 0 - val;
+                default:
+                    Debugger.Break();
+                    break;
+            }
+            return 0;
+        }
+    }
+}
